879cd0e190580c44d417c8a79a25798cf9e7e43e
Chris W Jones <chris@christopherjones.us>
2015-07-01 16:45:04 -0400

    Created interface for linked list
    I'm giving what I consider the bare minimum methods for a linked list:
    
    - next
    - prev
    - insert
    - delete
    - search

---
+++ hw2/src/hw2/LinkedListInterface.java
@@ -0,0 +1,9 @@
+package hw2;
+
+public interface LinkedListInterface {
+	public char next();
+	public char prev();
+	public long insert(char c);
+	public Boolean delete();
+	public char search();
+}
