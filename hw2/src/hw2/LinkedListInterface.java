6021abd61b13239275cbe37246ff5726c81b594d
Chris W Jones <chris@christopherjones.us>
2015-07-05 18:45:20 -0400

    Adds new method declarations
    I added the method declarations that allow the callbacks using the
    Runnable class.  I also added the NodeNotFound exception for the searches.

--- hw2/src/hw2/LinkedListInterface.java
+++ hw2/src/hw2/LinkedListInterface.java
@@ -1,7 +1,12 @@
 package hw2;
 
+import hw2.LinkedList.NodeNotFound;
+
 public interface LinkedListInterface {
 	public Node insert(Node newNode);
-	public void delete(Node deletableNode); 
-	public Node search(Node searchNode);
+	public Node insert(Node newNode, Runnable callback);
+	public void delete(Node deletableNode);
+	public void delete(Node deletableNode, Runnable callback);
+	public Node search(Node searchNode) throws NodeNotFound;
+	public Node search(Node searchNode, Runnable callback) throws NodeNotFound;
 }
