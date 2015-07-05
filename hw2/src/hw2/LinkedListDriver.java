891e4c6f026f8f121f8d862d4d7a02e6b1c07918
Chris W Jones <chris@christopherjones.us>
2015-07-05 18:50:04 -0400

    Wrote most of driver code
    In accordance with the homework instructions, this goes through and adds
    Nodes, searchs for one, deletes one, then inserts another one.

--- hw2/src/hw2/LinkedListDriver.java
+++ hw2/src/hw2/LinkedListDriver.java
@@ -1,9 +1,41 @@
 package hw2;
 
-public class LinkedListDriver {
+import hw2.LinkedList.NodeNotFound;
 
+import java.awt.print.Printable;
+
+public class LinkedListDriver {	
 	public static void main(String[] args) {
+		
 		LinkedList ll = new LinkedList();
+
+		/*
+		 * This is a method that can be passed to the insert(), delete(), 
+		 * or search() methods which will in turn call it.   
+		 */
+		Runnable printList = new Runnable(){
+			@Override
+			public void run(){
+				Node nextNode = ll.head();
+				int counter = 1;
+				StringBuilder output = new StringBuilder();
+				
+				output.append("Node\tPrev\tValue\tNext\n");
+				while(nextNode != ll.tail().next()){
+					output.append(counter++ + "\t");
+					output.append(nextNode.prev().value() + "\t"); 
+					output.append(nextNode.value() + "\t");
+					output.append(nextNode.next().value() + "\n");
+					nextNode = nextNode.next();
+				}
+				output.append(counter++ + "\t");
+				output.append(nextNode.prev().value() + "\t"); 
+				output.append(nextNode.value() + "\t");
+				output.append(nextNode.next().value() + "\n");
+				System.out.println(output.toString());
+			}
+		};
+		
 		Node[] nodes = {
 			new Node('a'),
 			new Node('b'),
@@ -12,12 +44,29 @@ public class LinkedListDriver {
 			new Node('e')
 		};
 		
+		// Push nodes onto the head of the list 
 		for (Node node : nodes) {
-			ll.insert(node);
+			ll.insert(node, printList);
 		}
 		
-		Node searchResults = ll.search(nodes[3]);
-		ll.delete(searchResults);
-	}
-
+		// Insert a node after a specified node
+		try {
+			ll.insert(new Node("aa"), nodes[1], printList);
+		} catch (NodeNotFound e) {
+			e.printStackTrace();
+		}
+		
+		
+		// I picked the first element to find and delete since that's an edge case. 
+		Node searchResults;
+		try {
+			searchResults = ll.search(new Node("g"), printList);
+			ll.delete(searchResults, printList);
+		} catch (NodeNotFound e) {
+			e.printStackTrace();
+		}
+		
+		// Just to show that the list operates correctly after a delete.
+		ll.insert(new Node('f'), printList);
+	};
 }
