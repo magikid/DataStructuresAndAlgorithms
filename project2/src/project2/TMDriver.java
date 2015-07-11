70343c274dd82e7f30f3193372741e309104b82b
Chris W Jones <chris@christopherjones.us>
2015-07-11 16:38:07 -0400

    Updated driver class to output
    I started working on the printing out the things required by the
    assignment.  So far, just the tree, search and height.

--- project2/src/project2/TMDriver.java
+++ project2/src/project2/TMDriver.java
@@ -1,15 +1,51 @@
 package project2;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 public class TMDriver {
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		RedBlackTree tree = new RedBlackTree();
+		List<Integer> nums = new ArrayList<Integer>();
 		
 		// Insert 21 random elements
-		for(int i=0; i<21; i++){
-			tree.insert(randInt());
-		}		
+		nums.add(randInt());
+		for(int i=0; i<20; i++){
+			int randomNumber = randInt();
+			while(nums.contains(randomNumber)){
+				randomNumber = randInt();
+			}
+			nums.add(randomNumber);
+			System.out.println("nums[" + i + "]=" + nums.get(i));
+		}
+			
+		
+		
+		for (int i : nums) {
+			tree.insert(i);
+		}
+		
+		// Assignment functions
+		System.out.println("\nInorder print");
+		System.out.println("=============");
+		tree.printTree();
+		System.out.println("\nSearches");
+		System.out.println("========");
+		System.out.println("Finding number in tree: " + nums.get(1));
+		if(tree.find(nums.get(1)) == nums.get(1)){
+			System.out.println("Found it! got: " + nums.get(1));
+		}
+		
+		System.out.println("Finding number not in tree: 999");
+		if(tree.find(999)==null){
+			System.out.println("Not found, null returned");
+		}
+		
+		System.out.println("\nHeight");
+		System.out.println("======");
+		System.out.println(tree.height());
 	}
 	
 	public static int randInt() {
