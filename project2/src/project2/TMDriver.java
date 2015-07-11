d0e6337635351019f5d3494135edeed8acf33997
Chris W Jones <chris@christopherjones.us>
2015-07-11 14:20:29 -0400

    Initial commit.  Code successfully stolen.

---
+++ project2/src/project2/TMDriver.java
@@ -0,0 +1,25 @@
+package project2;
+
+import java.util.Random;
+import java.util.TreeMap;
+
+public class TMDriver {
+	public static void main(String[] args) {		
+		RedBlackTree tree = new RedBlackTree();
+		
+		// Insert 21 random elements
+		for(int i=0; i<21; i++){
+			tree.insert(randInt());
+		}
+		
+		
+	}
+	
+	public static int randInt() {
+		int min=0, max=100;
+	    Random rand = new Random();
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
+	}
+}
