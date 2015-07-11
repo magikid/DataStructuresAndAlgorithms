e760aef6270efb29a2b4a312c3c874f2fe7e83e2
Chris W Jones <chris@christopherjones.us>
2015-07-11 19:54:40 -0400

    Updates for new data structure
    I'm not using the old RedBlackTree library so I'm updating the Driver for
    the new library.

--- project2/src/project2/TMDriver.java
+++ project2/src/project2/TMDriver.java
@@ -7,19 +7,22 @@ import java.util.Random;
 
 public class TMDriver {
 	public static void main(String[] args) {
-		RedBlackTree tree = new RedBlackTree();
+		RedBlackTree<Integer> tree = new RedBlackTree<Integer>();
 		List<Integer> nums = new ArrayList<Integer>();
+		StringBuilder finalOutput = new StringBuilder();
 		
 		// Insert 21 random elements
 		nums.add(randInt());
+		finalOutput.append("Initial numbers added to tree: [ ");
 		for(int i=0; i<20; i++){
 			int randomNumber = randInt();
 			while(nums.contains(randomNumber)){
 				randomNumber = randInt();
 			}
 			nums.add(randomNumber);
-			System.out.println("nums[" + i + "]=" + nums.get(i));
+			finalOutput.append(nums.get(i) + " ");
 		}
+		finalOutput.append("]\n\n");
 			
 		
 		
@@ -27,25 +30,43 @@ public class TMDriver {
 			tree.insert(i);
 		}
 		
-		// Assignment functions
-		System.out.println("\nInorder print");
-		System.out.println("=============");
-		tree.printTree();
-		System.out.println("\nSearches");
-		System.out.println("========");
-		System.out.println("Finding number in tree: " + nums.get(1));
-		if(tree.find(nums.get(1)) == nums.get(1)){
-			System.out.println("Found it! got: " + nums.get(1));
+		// Functions for homework
+		finalOutput.append("Inorder print\n");
+		finalOutput.append("=============\n");
+		finalOutput.append(tree.printTree("order") + "\n\n");
+		
+		finalOutput.append("Searches\n");
+		finalOutput.append("========\n");
+		finalOutput.append("Finding number in tree: " + nums.get(1) + "\n");
+		if(tree.search(nums.get(1)).key == nums.get(1)){
+			finalOutput.append("Found it! got: " + nums.get(1) + "\n");
 		}
 		
-		System.out.println("Finding number not in tree: 999");
-		if(tree.find(999)==null){
-			System.out.println("Not found, null returned");
+		finalOutput.append("Finding number not in tree: 999\n");
+		if(tree.search(999)==null){
+			finalOutput.append("Not found, null returned\n\n");
 		}
 		
-		System.out.println("\nHeight");
-		System.out.println("======");
-		System.out.println(tree.height());
+		finalOutput.append("Stats\n");
+		finalOutput.append("======\n");
+		finalOutput.append("Height: " + tree.height() + "\n");
+		finalOutput.append("Number leaves: " + tree.leaves().size() + "\n");
+		finalOutput.append("Number internal nodes: " + tree.numInternalNodes() + "\n\n");
+		finalOutput.append("Inserting\n");
+		finalOutput.append("=========\n");
+		finalOutput.append("new RedBlackNode(key=101)\n");
+		tree.insert(101);
+		finalOutput.append(tree.printTree() + "\n");
+		finalOutput.append("New Height: " + tree.height() + "\n\n");
+		
+		finalOutput.append("Different traversals\n");
+		finalOutput.append("====================\n");
+		finalOutput.append("In order: " + tree.printTree("order") + "\n");
+		finalOutput.append("Preorder: " + tree.printTree("pre") + "\n");
+		finalOutput.append("Postorder: " + tree.printTree("post") + "\n");
+		finalOutput.append("New number leaves: " + tree.leaves().size() + "\n");
+		finalOutput.append("Number internal nodes: " + tree.numInternalNodes() + "\n\n");
+		System.out.println(finalOutput.toString());
 	}
 	
 	public static int randInt() {
